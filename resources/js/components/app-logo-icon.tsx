908c388e1a05dee3c3bd8c78ae2aae1df7f627da
John Leward Escote <jleward.escote17@gmail.com>
2026-08-23 23:32:43 +0800

    chore: add dark mode logo

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -3,5 +3,5 @@ import type { ImgHTMLAttributes } from 'react';
 export default function AppLogoIcon(
     props: ImgHTMLAttributes<HTMLImageElement>,
 ) {
-    return <img {...props} src="/logo.png" alt="" />;
+    return <img {...props} src="/logo-dark-mode.png" alt="" />;
 }
